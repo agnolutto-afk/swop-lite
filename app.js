a941f1d3c389297b343eed22af273b6d2fe63936
agnolutto-afk <axelagnolutto@gmail.com>
2026-01-08 23:03:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("SWOP Lite OK");
+function $(id) {
+  return document.getElementById(id);
+}
+
+function show(view) {
+  ["view-scan", "view-runes", "view-optimizer"].forEach(v => {
+    $(v).classList.remove("active");
+  });
+  view.classList.add("active");
+
+  ["btnScan", "btnRunes", "btnOpti"].forEach(b => {
+    $(b).classList.remove("active");
+  });
+}
+
+$("btnScan").onclick = () => {
+  show($("view-scan"));
+  $("btnScan").classList.add("active");
+};
+
+$("btnRunes").onclick = () => {
+  show($("view-runes"));
+  $("btnRunes").classList.add("active");
+};
+
+$("btnOpti").onclick = () => {
+  show($("view-optimizer"));
+  $("btnOpti").classList.add("active");
+};
+
+$("scanInput").addEventListener("change", e => {
+  alert(e.target.files.length + " image(s) sélectionnée(s)");
+});
